e8607c67e953eded69e40f4ec8aa8f285d2655cc
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-07-09 11:40:40 +0200

    styles page results

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,17 +1,23 @@
 import { Image, Text, View } from "react-native";
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootParamList } from "../routes";
 
-type Props = NativeStackScreenProps<RootParamList, 'Results'>;
+type Props = NativeStackScreenProps<RootParamList, "Results">;
 
-const trophyUrl = 'https://arenafanzone.com/cdn/shop/products/1copie.jpg';
+const trophyUrl = "https://arenafanzone.com/cdn/shop/products/1copie.jpg";
 export const Results = ({ navigation, route }: Props) => {
   const { startingNumber, targetNumber, isHigher } = route?.params || {};
-  const isWin = !isHigher && startingNumber > targetNumber || isHigher && startingNumber < targetNumber;
+  const isWin =
+    (!isHigher && startingNumber > targetNumber) ||
+    (isHigher && startingNumber < targetNumber);
 
-  if (startingNumber === undefined || targetNumber === undefined || typeof isHigher !== 'boolean') {
+  if (
+    startingNumber === undefined ||
+    targetNumber === undefined ||
+    typeof isHigher !== "boolean"
+  ) {
     return (
       <View style={styles.container}>
         <Text>Invalid game results</Text>
@@ -26,28 +32,45 @@ export const Results = ({ navigation, route }: Props) => {
     } else {
       return `You've lost`;
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>You chose {isHigher ? 'Higher' : 'Lower'}</Text>
-      <Text>{resultMessage()}</Text>
-      <Text>Starting number was {startingNumber} and target was {targetNumber}</Text>
+      <Text style={styles.text}>You chose {isHigher ? "Higher" : "Lower"}</Text>
+      <Text style={styles.textBold}>{resultMessage()}</Text>
+      <Text style={styles.text}>
+        Starting number was {startingNumber} and target was {targetNumber}
+      </Text>
       {isWin && (
         <View>
-          <Image source={{ uri: trophyUrl }} style={{ width: 100, height: 100 }} />
+          <Image
+            source={{ uri: trophyUrl }}
+            style={{ width: 240, height: 240, marginTop: 50 }}
+          />
         </View>
       )}
       <StatusBar />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  text: {
+    fontSize: 30,
+    marginVertical: 15,
+    textAlign:"center",
+  },
+    textBold: {
+    fontSize: 30,
+    marginVertical: 15,
+    textAlign:"center",
+    fontWeight:"bold",
   },
 });
